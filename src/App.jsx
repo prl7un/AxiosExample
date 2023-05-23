e389f8141b07ca0920c0fec2b0583b9b4aebe0a0
prl7un <prl7un@naver.com>
2023-05-24 01:16:32 +0900

    타이머 초기버전

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,16 @@ import React, { useEffect, useState} from 'react';
 
 
 function App() {
-  const [num, setNum] = useState(5);
+  const [num, setNum] = useState(100);
+  useEffect(()=>{
+    setInterval(()=>{
+      setNum(num=>num-1);
+    }, 1000)
+  }, [])
 
-  
   return (
-    <div style={(num%2) ? {backgroundColor: 'green'} : {backgroundColor : 'red'}}>  
+    <div style={(num%4) ? {backgroundColor: 'green'} : {backgroundColor : 'red'}}
+    >  
       <button onClick={() =>setNum(num+1)}>click</button>
       <h1>{(num+1)%6}{(num+2)%6}{(num+3)%6}</h1>
       <h1>{(num+2)%6}{(num+3)%6}{(num+4)%6}</h1>
@@ -14,8 +19,7 @@ function App() {
       <h1>{(num-1)%6}{(num)%6}{(num+1)%6}</h1>
       <h1>{(num-2)%6}{(num-1)%6}{(num)%6}</h1>
       <h1>{(num-3)%6}{(num-2)%6}{(num-1)%6}</h1>
-      <h3></h3>
-      <h3></h3>
+      <h2>{num}</h2>
     </div>
   );
 }
