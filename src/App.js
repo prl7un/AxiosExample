8291b89743610f28317c2ca4da071b9d4ca28b44
prl7un <prl7un@naver.com>
2023-05-24 23:36:11 +0900

    리듀서 사용

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
-import React, { useState} from 'react';
+import React, { useState , useReducer} from 'react';
 
+export const initial_state = {
+  isOffline: false,
+  age: 30,
+  skilscore:10 
+}
+
+export const Reducer = (state=initial_state, action) => {
+  switch (action.type){
+    case "one":
+      return {
+
+      };
+    case "one":
+      return {
+
+      };
+    case "one":
+      return {
+
+      };
+    default:
+      return state;
+  }
+};
 
 function App() {
-  const [num, setNum] = useState(0);
-  const style = {
-    color: "red",
-    width:`${num}px`,
-    height:`${300-num}px`
-  }
-  
+  const [state, dispatch] = useReducer(Reducer, initial_state);
+
   return (
-    <div style={style}>  
-      <button onClick={setNum(num+1)}>click</button>
+    <div>  
+      <h1>memeber list</h1>
+      <p></p>
+      <button onClick={}>합류</button>
+      <button onClick={}>휴식</button>
     </div>
   );
 }
