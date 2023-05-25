7d6acda88f74f7e99923831765aead8450f03794
prl7un <prl7un@naver.com>
2023-05-25 22:26:42 +0900

    리듀서활용

--- src/App.js
+++ src/App.js
@@ -5,18 +5,17 @@ export const initial_state = {
   age: 30,
   skilscore:10 
 }
-
-export const Reducer = (state=initial_state, action) => {
+const Reducer = (state, action) => {
   switch (action.type){
     case "ONE":
       return {
         ...state,
-        isOffline: !isOffline
+        isOffline: !state.isOffline
       };
     case "TWO":
       return {
         ...state,
-        skilscore: skilscore+1
+        skilscore: state.skilscore+1
       };
     default:
       return state;
@@ -28,10 +27,11 @@ function App() {
 
   return (
     <div>  
-      <h1>memeber list</h1>
-      <p></p>
-      <button onClick={dispatch({type:ONE, })}>합류</button>
-      <button onClick={dispatch({type:TWO, })}>휴식</button>
+      <h1>아카데미</h1>
+      <button onClick={()=>dispatch({type:"ONE"})}>출입</button>
+      <button onClick={()=>dispatch({type:"TWO" })}>스킬업</button>
+      <p>{state.isOffline ? "입장": "퇴장"}</p>
+      <p>스킬점수{state.skilscore}</p>
     </div>
   );
 }
