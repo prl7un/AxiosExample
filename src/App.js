9fe7fded57d691e8efa252a4c0cc52469273f676
prl7un <prl7un@naver.com>
2023-05-27 23:16:08 +0900

    급여 표시판

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
-import React, { useCallback, useState } from 'react';
-import Ramen from './Ramen'
-import Gimbab from './Gimbab';
+import React from 'react';
+import useSlary from './useSalary'
+
 
 function App() {
-  const [orderNumber, setOrderNumber] = useState(0);
-
-  const incrementOrderNumber = () => {
-    setOrderNumber(orderNumber + 1);
-  };
-
-  
-  const order = useCallback(()=>{
-    console.log('주문완료');
-  }, []);
-  
+  const [person1 ,mon1, year1] = useSlary('A', 200);
+  const [person2 ,mon2, year2] = useSlary('B', 300);
   return (
-    <div style ={{ border: '2px solid grey', padding: '15px'}}>  
-      <h1>메뉴판</h1> 
-      <p>주문인원: {orderNumber}</p>
-      <button onClick={incrementOrderNumber}>주문인원추가</button>
-
-      <Ramen name={"치즈"} o={order}/>
-      <Gimbab name={"새우"} o={order}/>
+    <div>  
+      <label>
+        information:
+        <p>{person1}의 월급 {mon1}, 연봉 {year1}</p>
+      </label>
+      <label>
+        information:
+        <p>{person2}의 월급 {mon2}, 연봉 {year2}</p>
+      </label>
     </div>
   );
 }
