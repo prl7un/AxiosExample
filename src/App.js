3fff173cc7f4f662234cc938db807151f547de97
prl7un <prl7un@naver.com>
2023-05-29 17:37:05 +0900

    useRef활용

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useRef} from "react";
+import { useRef, useState} from "react";
 
 
 function App() {
@@ -10,12 +10,21 @@ function App() {
     console.log(R.current)
   }
 
+  const t_Ref = useRef('');
+
+  const [value, setValue] = useState('');
+  const cha = (e) => {
+    setValue(e.target.value);
+  }
+
   return (
     <div> 
       <hr></hr>
       <p>{R.current}</p>
       <button onClick={mult}>Ref변화</button>
       <hr></hr>
+      <input type="text" ref={t_Ref} onChange={cha}/>
+      <p>{t_Ref.current}</p>
     </div>
   );
 }
