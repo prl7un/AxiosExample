8f189ba9dbf498a5b9ce0836085e344db3898a97
prl7un <prl7un@naver.com>
2023-06-05 22:48:03 +0900

    튜플push

--- src/a.ts
+++ src/a.ts
@@ -1,3 +1,4 @@
 let arr1: (string|number)[] =['asefasef', 3452351, 523531243214]
 let arr2: ('아침'|'점심'|'저녁')[] = ['점심', '아침' , '저녁']
 let arr3: [number, string, string, boolean] = [2352424, '23452345', '3523lkjlk', true]
+arr3.push(false);
