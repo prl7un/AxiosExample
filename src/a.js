cc69d65976b1d05a2a8fd5eb4a28cb7d1a7ce1f9
prl7un <prl7un@naver.com>
2023-06-29 23:55:17 +0900

    axios활용

---
+++ src/a.js
@@ -0,0 +1,8 @@
+import axios from "axios"
+
+async function a(){
+  await axios.get('https://jsonplaceholder.typicode.com/comments/5')
+  .then((response)=>console.log(response.data.name))
+}
+
+a()
